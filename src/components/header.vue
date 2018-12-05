f02a1abf8767bd9068c3fd5be454893506249fbe
zhangfeng <742452836@qq.com>
2018-12-05 21:05:11 +0800

    555

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <h1 class="box">加代码12块</h1>
+    <ul>
+        <li>2</li>
+        <li>2</li>
+        <li>2</li>
+ 
+    </ul>
+    </div>
+</template>
+<style lang="less" scoped>
+@h:100px;
+.box{width: @h;background: red;}
+</style>
